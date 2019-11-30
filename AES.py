492ca017b5e27aeafea5c154b6732e2bbc1ae709
ARCbyG <ARCbyG@gmail.com>
2019-11-30 22:58:37 +0900

    E_Key Scheduling

--- AES.py
+++ AES.py
@@ -1,23 +1,72 @@
-def AES(InputBytes, KeyBytes, mode:'E' or 'D'): # 16진수인 스트링으로 받아서 state로 변환 후 암호화 진행
-    if mode == 'E':
-        I = state(InputBytes)
-        K = state(KeyBytes)
-        I.outMain()
-        I.outPrint()
-        K.outMain()
-        K.outPrint()
+import sys
 
-    elif mode == 'D':
-        pass
-
+def AES(InputBytes:str, KeyBytes:str, mode:'E'or'D', Version:'A'or'B'or'C'='A'): # 16진수인 스트링으로 받아서 state로 변환 후 암호화 진행
+    if Version == 'A':
+        Bit = 128
+        Rnd = 10
     else:
-        print("AES 모드가 설정되지 않았습니다.")
+        print("%s의 옵션이 틀렸습니다. :%s"%(sys._getframe().f_code.co_name, Version))
         return -1
 
-class state:
+    if mode == 'E':
+        print('%s %dbit Encrypting Start\n'%(sys._getframe().f_code.co_name, Bit))
+        I = state(InputBytes)
+        K = keys(state(KeyBytes),Rnd)
+        I.outPrint()
+        K.out()
+        
+
+    elif mode == 'D':
+        print('%s %dbit Decrypting Start\n'%(sys._getframe().f_code.co_name, Bit))
+        
+
+    else:
+        print("%s의 옵션이 틀렸습니다. :%s"%(sys._getframe().f_code.co_name, mode))
+        return -1
+
+class Table:
+    Sbox = [0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+            0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+            0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+            0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+            0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+            0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+            0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+            0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+            0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+            0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+            0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+            0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+            0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+            0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+            0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+            0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16]
     
-    def __init__(self, initValue):
+    Ibox = [0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+            0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+            0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+            0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+            0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+            0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+            0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+            0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+            0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+            0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+            0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+            0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+            0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+            0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+            0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+            0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d]
+
+    # 초기값 0x01. 직전값*2 (직전 값이 0x80 이상일 시엔 xor 0x11B연산을 추가.)
+    Rcon = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36] # 사용하는 워드 / 워드길이 = 필요한 RC수. 128bit에선 44/4 10, 192: 52/6 = 8, 256: 60/8 = 7개 사용.
+
+class state:
+    def __init__(self, initValue:str=""):
         S = initValue.split()
+        while len(S) < 16:  # 패딩: 남는 부분 0으로 채우기
+            S.append('0')
 
         self.Aa = int(S[0], 16)
         self.Ba = int(S[1], 16)
@@ -55,27 +104,61 @@ class state:
                            self.Ba, self.Bb, self.Bc, self.Bd,
                            self.Ca, self.Cb, self.Cc, self.Cd,
                            self.Da, self.Db, self.Dc, self.Dd]   # 출력시
-        '''
-    def outMain(self):
+        
+    def outMain(self):  # 이 파이썬 코드에선 어차피 모든 원소가 한 행렬이 아닌 각각의 변수라서 의미가 없다. 향후 C코드로 넘어가 최적화를 할 때 쓸 아이디어.
         print("\n%3x%3x%3x%3x\n%3x%3x%3x%3x\n%3x%3x%3x%3x\n%3x%3x%3x%3x\n\n"%
                          (self.Aa, self.Ba, self.Ca, self.Da,
                           self.Ab, self.Bb, self.Cb, self.Db,
                           self.Ac, self.Bc, self.Cc, self.Dc,
                           self.Ad, self.Bd, self.Cd, self.Dd))    # 계산시
+        '''
     def outPrint(self):
-        print("\n%3x%3x%3x%3x\n%3x%3x%3x%3x\n%3x%3x%3x%3x\n%3x%3x%3x%3x\n\n"%
+        print("%3x%3x%3x%3x\n%3x%3x%3x%3x\n%3x%3x%3x%3x\n%3x%3x%3x%3x\n"%
                           (self.Aa, self.Ab, self.Ac, self.Ad,
                            self.Ba, self.Bb, self.Bc, self.Bd,
                            self.Ca, self.Cb, self.Cc, self.Cd,
                            self.Da, self.Db, self.Dc, self.Dd))   # 출력시
 
+class keys:
+    def __init__(self, initValue:state, Needs:int):  # 매개변수 기본값과 주석을 동시에 사용할 경우 주석이 먼저.
+        self.S = [] # S는 클래스 내부에서만 쓰임. 출력을 따로 함수로 정의해줬기 때문에 어차피 사용자는 모르는 변수.
+        self.S.append(initValue)
+        for j in range(Needs):
+            self.S.append(state())
+        for i in range(Needs):
+            self.S[i+1].Aa = self.S[i].Aa^Table.Sbox[self.S[i].Bd]^Table.Rcon[i]
+            self.S[i+1].Ba = self.S[i].Ba^Table.Sbox[self.S[i].Cd]
+            self.S[i+1].Ca = self.S[i].Ca^Table.Sbox[self.S[i].Dd]
+            self.S[i+1].Da = self.S[i].Da^Table.Sbox[self.S[i].Ad]
+            
+            self.S[i+1].Ab = self.S[i].Ab^self.S[i+1].Aa
+            self.S[i+1].Bb = self.S[i].Bb^self.S[i+1].Ba
+            self.S[i+1].Cb = self.S[i].Cb^self.S[i+1].Ca
+            self.S[i+1].Db = self.S[i].Db^self.S[i+1].Da
+            
+            self.S[i+1].Ac = self.S[i].Ac^self.S[i+1].Ab
+            self.S[i+1].Bc = self.S[i].Bc^self.S[i+1].Bb
+            self.S[i+1].Cc = self.S[i].Cc^self.S[i+1].Cb
+            self.S[i+1].Dc = self.S[i].Dc^self.S[i+1].Db
         
-
+            self.S[i+1].Ad = self.S[i].Ad^self.S[i+1].Ac
+            self.S[i+1].Bd = self.S[i].Bd^self.S[i+1].Bc
+            self.S[i+1].Cd = self.S[i].Cd^self.S[i+1].Cc
+            self.S[i+1].Dd = self.S[i].Dd^self.S[i+1].Dc
+    
+    def out(self, num:int=-1):
+        if num > len(self.S) or num < 0:    # 모두 출력
+            for i in range(len(self.S)):
+                print('%d번째 확장키'%(i))
+                self.S[i].outPrint()
+        else:
+            print('%d번째 확장키'%num)
+            self.S[num].outPrint()
 
 def main():
     AES("32 43 f6 a8 88 5a 30 8d 31 31 98 a2 e0 37 07 34", "2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c", 'E')
-    #print("%x"%int("32", 16))
-    
+    #AES("32 43 f6 a8", "2b 7e 15 16", 'E')
+
 
 if __name__ == "__main__":
     main()
@@ -89,5 +172,88 @@ a = int(a,16)
 print("32", a, 0x32)
 print(len("32")*16)
 print(sys.getsizeof("32"), sys.getsizeof(a), sys.getsizeof(0x32))
+
+#print("%x"%int("32", 16))
+
+
+# 패딩 코드
+initValue = "aa bb cc dd"
+K = initValue.split()
+print(K, len(K))
+while len(K) < 16:
+        K.append('0')
+print(K, len(K))
+
+
+# SBox를 위한 두자리 수 가르기 코드 -> 근데 sbox를 행렬로 구현하면 필요가 없네..    print(Sbox.Sbox[0xcf])
+C = state("32 43 f6 a8")
+C.outPrint()
+print(C.Aa, hex(C.Aa))
+print(C.Aa//16, C.Aa%16)
+
+# xor 연산 테스트
+C = state("2b 7e 15 16")
+C.outPrint()
+print(C.Aa, hex(C.Aa))
+C.Aa = C.Aa^0x8a
+C.outPrint()
+print(C.Aa, hex(C.Aa))
+
+
+# 키 확장 테스트 : 함수의 확장 키 출력 부분 주석 해제하면 각각 출력됨
+aa = state("2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c")
+K = KeyExtended(aa, 'E')
+
+
+def KeyExtended(Key:state, mode:'E' or 'D'): # 함수보다 클래스가 나을 것 같아 변경. 키 확장 -> 라운드 키 생성
+    # state 클래스 정의보다 뒤에 있어야 Key:주석에서 오류가 나지 않음. 번역과정에서 해석돼서 그런듯. AES는 인자값이 아닌 내부코드에서 사용이라 런타임에서 실행돼서 괜찮은걸로 예상됨.
+    if mode == 'E':
+        Keys = []
+        for i in range(11):
+            Keys.append(state())
+        Keys[0] = Key
+        for i in range(10): # 128bit는 10개의 확장키가 필요함. Keys[0]은 초기키. Keys[10]까지 생성.
+            Keys[i+1].Aa = Keys[i].Aa^Table.Sbox[Keys[i].Bd]^Table.Rcon[i]
+            # 값이 달라서 체크했던 코드. 원인: Bd가 Bb로 잘못 들어가있었음.
+            # print(hex(Keys[i].Bd), ':', hex(Table.Sbox[Keys[i].Bd]))
+            # print(hex(Keys[i].Aa), hex(Table.Sbox[Keys[i].Bd]), hex(Table.Rcon[i]), ':', hex(Keys[i+1].Aa))
+            Keys[i+1].Ba = Keys[i].Ba^Table.Sbox[Keys[i].Cd]
+            Keys[i+1].Ca = Keys[i].Ca^Table.Sbox[Keys[i].Dd]
+            Keys[i+1].Da = Keys[i].Da^Table.Sbox[Keys[i].Ad]
+            
+            Keys[i+1].Ab = Keys[i].Ab^Keys[i+1].Aa
+            Keys[i+1].Bb = Keys[i].Bb^Keys[i+1].Ba
+            Keys[i+1].Cb = Keys[i].Cb^Keys[i+1].Ca
+            Keys[i+1].Db = Keys[i].Db^Keys[i+1].Da
+            
+            Keys[i+1].Ac = Keys[i].Ac^Keys[i+1].Ab
+            Keys[i+1].Bc = Keys[i].Bc^Keys[i+1].Bb
+            Keys[i+1].Cc = Keys[i].Cc^Keys[i+1].Cb
+            Keys[i+1].Dc = Keys[i].Dc^Keys[i+1].Db
+        
+            Keys[i+1].Ad = Keys[i].Ad^Keys[i+1].Ac
+            Keys[i+1].Bd = Keys[i].Bd^Keys[i+1].Bc
+            Keys[i+1].Cd = Keys[i].Cd^Keys[i+1].Cc
+            Keys[i+1].Dd = Keys[i].Dd^Keys[i+1].Dc
+
+            # c확장키 출력
+            # print('%d번째 확장키'%(i+1))
+            # Keys[i+1].outPrint()
+        return Keys
+
+    elif mode == 'D':
+        pass
+
+    else:
+        print("%s의 옵션이 틀렸습니다. :%s"%(sys._getframe().f_code.co_name, mode))
+        return -1
+
+
+
+# 클래스 키 확장 테스트
+aa = state("2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c")
+K = keys(aa)
+# print(K.out())
+K.out()
 
 """
